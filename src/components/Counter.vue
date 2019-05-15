d1473e41232ab926c854b6ec09660035745400a0
Leandro <vitto.leandro@gmail.com>
2019-05-15 17:37:12 +0200

    Init Project

---
+++ src/components/Counter.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>
+    <span class="count" v-text="count"></span>
+    </div>
+    <div>
+    <button @click="count++" class="btn btn-dark">Counter ++</button>
+    </div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'Counter',
+  data () {
+    return {
+      count : 0
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.count{
+  font-size: 10em;
+}
+.btn{
+  font-size: 3em;
+}
+</style>
---
+++ src/views/Navigator.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="nav" >
+    <router-link :to="{ name : 'home'}">Home</router-link> |
+    <router-link :to="{ name : 'counter'}">Counter</router-link> |
+<!--     <router-link :to="{ name : 'newpost'}">New Post</router-link> |
+    <router-link :to="{ name : 'listpost'}">List Post</router-link> | -->
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'navigator',
+  data (){
+    return {
+    }
+  },
+  computed : {
+
+  }
+}
+</script>
+
+
+
