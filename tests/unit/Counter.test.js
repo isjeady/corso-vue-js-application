db591292639435be8403ecb3f71a7b322cc8d1fb
Leandro <vitto.leandro@gmail.com>
2019-05-15 18:05:45 +0200

    Primo test del Counter

--- tests/unit/Counter.test.js
+++ tests/unit/Counter.test.js
@@ -1,12 +1,11 @@
 import { shallowMount } from '@vue/test-utils'
-import HelloWorld from '@/components/HelloWorld.vue'
+import Counter from '@/components/Counter.vue'
 
-describe('HelloWorld.vue', () => {
-  it('renders props.msg when passed', () => {
-    const msg = 'new message'
-    const wrapper = shallowMount(HelloWorld, {
-      propsData: { msg }
-    })
-    expect(wrapper.text()).toMatch(msg)
-  })
+describe('Counter.vue', () => {
+
+  it('inizio del counter con valore 0', () => {
+    const wrapper = shallowMount(Counter);
+    expect(wrapper.vm.count).toBe(0);
+  });
+
 })
