c43327e9c5dbb91d7aebd9197d44a4aeedb21549
Leandro <vitto.leandro@gmail.com>
2019-05-15 18:12:59 +0200

    Test Button e Value

--- tests/unit/Counter.test.js
+++ tests/unit/Counter.test.js
@@ -2,10 +2,30 @@ import { shallowMount } from '@vue/test-utils'
 import Counter from '@/components/Counter.vue'
 
 describe('Counter.vue', () => {
+  let wrapper;
+
+  beforeEach(() => {
+    wrapper = shallowMount(Counter);
+  });
 
   it('inizio del counter con valore 0', () => {
     const wrapper = shallowMount(Counter);
     expect(wrapper.vm.count).toBe(0);
   });
+  
+  it('incremento del contatore quando clicco', () => {
+    wrapper.find('button').trigger('click');
+    expect(wrapper.vm.count).toBe(1);
+  });
+
+  it('visualizzazione valore del contatore', () => {
+    expect(wrapper.find('.count').html()).toContain(0);
+    
+    wrapper.find('button').trigger('click');
+    
+    expect(wrapper.find('.count').html()).toContain(1);
+  });
+
+
 
 })
