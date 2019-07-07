fa6096554f5437fc0d593d5cba2252deb1a8e8ee
Unknown <vitto.leandro@gmail.com>
2019-07-07 14:01:57 +0200

    Filter & Mixins

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Test Jest"/>
+    <HelloWorld msg="Filter & Mixins"/>
   </div>
 </template>
 
